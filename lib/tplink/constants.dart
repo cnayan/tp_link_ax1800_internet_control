14b47376321b5bf53d44f05409e7891e9e8e8695
Nayan Choudhary <cnayan@gmail.com>
2025-07-29 16:22:56 +0530

    Sample

--- lib/tplink/constants.dart
+++ lib/tplink/constants.dart
@@ -7,13 +7,12 @@ class WhiteListedDevice {
 }
 
 class Constants {
-  static const String routerIP = '192.168.0.1';
-  static const String routerUser = 'admin';
-  static const String routerPassword = 'Sunny_81';
+  static const routerIP = <IP>;
+  static const routerUser = <USER>; //admin
+  static const routerPassword = <PASSWORD>;
 
   static final List<WhiteListedDevice> unblockableDevices = [
-    WhiteListedDevice("AA-F4-F7-E6-FD-FA", DeviceType.mobile),
-    WhiteListedDevice("E8-4E-06-4B-1F-5A", DeviceType.pc),
-    WhiteListedDevice("E0-D5-5E-2D-B8-74", DeviceType.pc),
+    WhiteListedDevice("XX-XX-XX-XX-XX-XX", DeviceType.mobile),
+    WhiteListedDevice("YY-YY-YY-YY-YY-YY", DeviceType.pc),
   ];
 }
